eb9424ba660370d5ccbcc3bc813ac8184f47b33e
Davi Braz <davibraz.profissional@gmail.com>
2026-01-17 18:32:43 -0300

    feat: implementando tecnico resource

---
+++ helpdesk-api/src/main/java/com/project/helpdesk/services/TecnicoService.java
@@ -0,0 +1,22 @@
+package com.project.helpdesk.services;
+
+import com.project.helpdesk.domain.Tecnico;
+import com.project.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
+
+        return tecnico.orElse(null);
+    }
+}

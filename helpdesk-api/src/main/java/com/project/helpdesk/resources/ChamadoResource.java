7d691713817799017337e8792b6f508d2aadf453
Davi Braz <davibraz.profissional@gmail.com>
2026-01-20 14:46:53 -0300

    feat: metodo para encontrar chamado por id

---
+++ helpdesk-api/src/main/java/com/project/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,26 @@
+package com.project.helpdesk.resources;
+
+import com.project.helpdesk.domain.Chamado;
+import com.project.helpdesk.domain.dtos.ChamadoDTO;
+import com.project.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService chamadoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+        Chamado chamado = chamadoService.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(chamado));
+    }
+
+}
---
+++ helpdesk-api/src/main/java/com/project/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.project.helpdesk.services;
+
+import com.project.helpdesk.domain.Chamado;
+import com.project.helpdesk.repositories.ChamadoRepository;
+import com.project.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> chamado = chamadoRepository.findById(id);
+        return chamado.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+}

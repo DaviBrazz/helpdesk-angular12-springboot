64bda21c6d361eb8f1d775942c71fcd896e22bb5
Davi Braz <davibraz.profissional@gmail.com>
2026-01-20 13:03:25 -0300

    feat: CRUD cliente implementação

--- helpdesk-api/src/main/java/com/project/helpdesk/domain/Cliente.java
+++ helpdesk-api/src/main/java/com/project/helpdesk/domain/Cliente.java
@@ -2,8 +2,11 @@ package com.project.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.helpdesk.domain.dtos.ClienteDTO;
+import com.project.helpdesk.domain.dtos.TecnicoDTO;
 import com.project.helpdesk.domain.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -27,6 +30,16 @@ public class Cliente extends Pessoa {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
 	}
+
+    public Cliente(ClienteDTO clienteDTO) {
+        this.id = clienteDTO.getId();
+        this.nome = clienteDTO.getNome();
+        this.cpf = clienteDTO.getCpf();
+        this.email = clienteDTO.getEmail();
+        this.senha = clienteDTO.getSenha();
+        this.perfis = clienteDTO.getPerfis().stream().map(cod -> cod.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = clienteDTO.getDataCriacao();
+    }
 
 	public List<Chamado> getChamados() {
 		return chamados;
